5bf6a8acf733047b3e016c644439ff299349009e
Usman Adepoju <adepojuusman465@gmail.com>
2023-12-12 09:28:32 +0100

    create and connect to mongodb database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
-import { errorHandler } from "./middleware/errorHandler";
+import { errorHandler } from "./middleware/errorHandler.js";
+import mongoose from "mongoose";
 dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_CONN_STRING)
+  .then(() => {
+    console.log("connected to MongoDB!!!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
