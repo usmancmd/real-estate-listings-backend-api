322289ce2fa87855fec676e48abe3d5531c8aaac
Usman Adepoju <adepojuusman465@gmail.com>
2023-12-12 16:50:12 +0100

    update imports

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { signIn, signOut, signUp } from "../controllers/auth.controller.js";
-import { verifyUser } from "../middleware/verifyUser.js";
 
 const router = express.Router();
 
