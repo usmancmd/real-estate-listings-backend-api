802a467d027f4725240a68c483b7204a1c0d7089
Usman Adepoju <adepojuusman465@gmail.com>
2023-12-12 08:47:50 +0100

    configure error handler middleware with express app

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { errorHandler } from "./middleware/errorHandler";
 dotenv.config();
 
 const app = express();
@@ -7,3 +8,5 @@ const app = express();
 app.listen(process.env.PORT, () => {
   console.log(`server running on port ${process.env.PORT}`);
 });
+
+app.use(errorHandler);
