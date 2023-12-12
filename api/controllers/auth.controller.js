6d7b0749b018dbea45aec65e73ccbae7ef25f371
Usman Adepoju <adepojuusman465@gmail.com>
2023-12-12 13:15:22 +0100

    create sign-in api controller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { customErrorHandler } from "../utils/error.js";
+import Jwt from "jsonwebtoken";
+import { response } from "express";
 
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -12,6 +15,28 @@ export const signUp = async (req, res, next) => {
     return res.status(201).json({
       response_msg: "user created successfully",
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(customErrorHandler(404, "wrong credentials!"));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword)
+      return next(customErrorHandler(404, "from passwd wrong credentials!"));
+
+    const token = Jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: passwd, ...response } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json({ response_msg: "user signed in successfully", response });
   } catch (error) {
     next(error);
   }
