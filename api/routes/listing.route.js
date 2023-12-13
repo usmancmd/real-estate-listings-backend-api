46c8b7a1fed7a953441428129118699194d2f45c
Usman Adepoju <adepojuusman465@gmail.com>
2023-12-13 20:52:43 +0100

    Add middlewares to create listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -2,14 +2,17 @@ import express from "express";
 import {
   createListing,
   deleteListing,
+  getListings,
   updateListing,
 } from "../controllers/listing.controller.js";
 import { verifyUser } from "../middleware/verifyUser.js";
+import { addUserRef } from "../middleware/addUserRef.js";
 
 const router = express.Router();
 
-router.post("/create", verifyUser, createListing);
+router.post("/create", verifyUser, addUserRef, createListing);
 router.put("/update/:id", verifyUser, updateListing);
 router.delete("/delete/:id", verifyUser, deleteListing);
+router.get("/", getListings);
 
 export default router;
