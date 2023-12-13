7222639b1837fb25675bf9c0d1e72d15dc5eaa24
Usman Adepoju <adepojuusman465@gmail.com>
2023-12-13 10:07:45 +0100

    Add update user and delete user api controller

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,47 @@
+import User from "../models/user.model.js";
+import { customErrorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(customErrorHandler(401, "Unathorized"));
+
+  try {
+    if (req.body.password) {
+      const salt = bcryptjs.genSaltSync(10);
+      req.body.password = bcryptjs.hashSync(req.body.password, salt);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+
+    const { password, ...response } = updatedUser._doc;
+    res.status(200).json({ message: "user updated successfully!", response });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(customErrorHandler(401, "Unathorized"));
+  }
+
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("access_token");
+    res.status(200).json({ message: "user deleted successfully!" });
+  } catch (error) {
+    next(error);
+  }
+};
