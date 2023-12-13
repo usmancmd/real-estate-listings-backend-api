6698dcf661ed908bf789c1c5b5721b650daee8dc
Usman Adepoju <adepojuusman465@gmail.com>
2023-12-13 12:07:51 +0100

    Add update listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { createListing } from "../controllers/listing.controller.js";
+import {
+  createListing,
+  updateListing,
+} from "../controllers/listing.controller.js";
 import { verifyUser } from "../middleware/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create", verifyUser, createListing);
+router.put("/update/:id", verifyUser, updateListing);
 
 export default router;
