b4a5b09ae43e638db14590ebe24e414ffa059d96
Usman Adepoju <adepojuusman465@gmail.com>
2023-12-13 20:48:51 +0100

    Add get listings and delete listing api controller

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -2,6 +2,7 @@ import Listing from "../models/listing.model.js";
 import { customErrorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
+  console.log(req.body.userRef);
   if (req.user.id !== req.body.userRef)
     return next(customErrorHandler(403, "Forbidden"));
 
@@ -12,6 +13,7 @@ export const createListing = async (req, res, next) => {
         .json({ message: "listing created successfully", listing });
     });
   } catch (error) {
+    console.log("f111111111111");
     next(error);
   }
 };
@@ -32,6 +34,56 @@ export const updateListing = async (req, res, next) => {
           .status(200)
           .json({ message: "listing updated successfully", updatedListing });
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) return next(customErrorHandler(404, "listing does not exist"));
+
+  if (req.user.id !== listing.userRef)
+    return next(customErrorHandler(401, "from id Unauthorized"));
+
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "listing deleted successfully!" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 6;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const name = req.query.name || "";
+    const description = req.query.description || "";
+    const address = req.query.address || "";
+
+    let { parking, furnished, type } = req.query;
+
+    if (furnished === undefined || furnished === "false")
+      furnished = { $in: [false, true] };
+
+    if (parking === undefined || parking === "false")
+      parking = { $in: [false, true] };
+
+    if (type === undefined) type = { $in: ["sale", "rent"] };
+
+    const listings = await Listing.find({
+      name: { $regex: name, $options: "i" },
+      description: { $regex: description, $options: "i" },
+      address: { $regex: address, $options: "i" },
+      furnished,
+      // parking,
+      // type,
+    })
+      .limit(limit)
+      .skip(startIndex);
+
+    return res.status(200).json(listings);
   } catch (error) {
     next(error);
   }
