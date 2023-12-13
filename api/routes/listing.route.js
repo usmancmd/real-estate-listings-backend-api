6d14707fa21fa5d364edd18eb0c9fcd6e1c3578e
Usman Adepoju <adepojuusman465@gmail.com>
2023-12-13 13:14:14 +0100

    Add delete listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   createListing,
+  deleteListing,
   updateListing,
 } from "../controllers/listing.controller.js";
 import { verifyUser } from "../middleware/verifyUser.js";
@@ -9,5 +10,6 @@ const router = express.Router();
 
 router.post("/create", verifyUser, createListing);
 router.put("/update/:id", verifyUser, updateListing);
+router.delete("/delete/:id", verifyUser, deleteListing);
 
 export default router;
