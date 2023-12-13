0563a019cedcec337d3579ef90c101eecd4aa226
Usman Adepoju <adepojuusman465@gmail.com>
2023-12-13 13:13:24 +0100

    Add get user listing and get user listings api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,17 @@
-import express from 'express'
-import { deleteUser, updateUser } from '../controllers/user.controller.js'
-import {verifyUser } from '../middleware/verifyUser.js'
+import express from "express";
+import {
+  deleteUser,
+  updateUser,
+  getUserListing,
+  getUserListings,
+} from "../controllers/user.controller.js";
+import { verifyUser } from "../middleware/verifyUser.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.put("/update/:id", verifyUser, updateUser)
-router.delete("/delete/:id", verifyUser, deleteUser)
+router.put("/update/:id", verifyUser, updateUser);
+router.delete("/delete/:id", verifyUser, deleteUser);
+router.get("/listing/:id", verifyUser, getUserListing); // requires listing id
+router.get("/listings/:id", verifyUser, getUserListings); // reqiures user id id
 
-export default router
+export default router;
