68607acc2436a7da48551747db5eaa290ddbb71c
Usman Adepoju <adepojuusman465@gmail.com>
2023-12-13 12:11:19 +0100

    Add update listing api controller

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -15,3 +15,24 @@ export const createListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) return next(customErrorHandler(404, "listing not found!"));
+
+  if (req.user.id !== listing.userRef)
+    return next(customErrorHandler(401, "Unauthorized"));
+
+  try {
+    await Listing.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    }).then((updatedListing) => {
+      if (updatedListing)
+        res
+          .status(200)
+          .json({ message: "listing updated successfully", updatedListing });
+    });
+  } catch (error) {
+    next(error);
+  }
+};
