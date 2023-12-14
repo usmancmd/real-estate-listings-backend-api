ee1c59828de4ad1093f0b9f84a5da18f4683f86d
Usman Adepoju <adepojuusman465@gmail.com>
2023-12-14 14:40:47 +0100

    update user ref middleware

--- api/middleware/addUserRef.js
+++ api/middleware/addUserRef.js
@@ -1,6 +1,3 @@
-import { customErrorHandler } from "../utils/error.js";
-import jwt from "jsonwebtoken";
-
 export const addUserRef = (req, res, next) => {
   try {
     req.body.userRef = req.user.id;
